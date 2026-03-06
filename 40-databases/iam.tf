4f9e79e7efe138c482232aed3779c7a93f687707
Sivakumar Reddy M <info@joindevops.com>
2026-03-06 08:07:30 +0530

    terraform

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -24,3 +24,9 @@ resource "aws_iam_role" "mysql" {
     local.common_tags
   )
 }
+
+resource "aws_iam_policy" "mysql" {
+  name        = "S3ReadOnlyAccessPolicy"
+  description = "A policy for MySQL Ec2 instance"
+  policy      = file("mysql-iam-policy.json")
+}
